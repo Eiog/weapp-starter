24ce76759bb105d4a9160df82e05d95af4f57a10
antrioe <antrioe@gmali.com>
2024-07-06 17:43:50 +0800

    init

---
+++ uno.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "unocss";
+import presetWeapp from 'unocss-preset-weapp'
+import { transformerClass } from 'unocss-preset-weapp/transformer';
+const include = [/\.wxml$/]
+
+export default defineConfig({
+  content:{
+    pipeline:{
+      include
+    }
+  },
+  presets: [
+    presetWeapp(),
+  ],
+  transformers:[
+    transformerClass({
+      include,
+      classTags: false
+    })
+  ],
+  separators:'__'
+})
